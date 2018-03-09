af0a75387bd5522c415cb56fb1e92134910c4c21
William <william2958@gmail.com>
2018-03-09 17:14:29 -0500

    Initialized Auth Application

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
